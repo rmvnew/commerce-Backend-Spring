306338447996b7275b39dbef587cbb3955bf7223
Ricardo <rmvnew@gmail.com>
2023-06-18 09:23:19 -0400

    chore: add product and category entities

---
+++ src/main/java/com/delta/commerce/entity/Category.java
@@ -0,0 +1,31 @@
+package com.delta.commerce.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity(name = "Category")
+@Table(name = "tb_category")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Product> products;
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
---
+++ src/main/java/com/delta/commerce/entity/Product.java
@@ -0,0 +1,89 @@
+package com.delta.commerce.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "Product")
+@Table(name = "tb_product")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private int productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_barcode")
+    private String productBarcode;
+
+    @Column(name = "product_code")
+    private String productCode;
+
+    @Column(name = "product_ncm")
+    private String productNcm;
+
+    @Column(name = "product_cfop")
+    private String productCfop;
+
+    @Column(name = "product_unit_of_measurement")
+    private String productUnitOfMeasurement;
+
+    @Column(name = "product_quantity")
+    private double productQuantity;
+
+    @Column(name = "product_minimum_stock")
+    private double productMinimumStock;
+
+    @Column(name = "product_unit_cost")
+    private BigDecimal productUnitCost;
+
+    @Column(name = "product_unit_price")
+    private BigDecimal productUnitPrice;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "create_at")
+    private LocalDateTime createAt;
+
+    @Column(name = "update_at")
+    private LocalDateTime updateAt;
+
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",nullable = false)
+    private Category category;
+
+    public Product(String productName, String productBarcode, String productCode,
+                   String productNcm, String productCfop, String productUnitOfMeasurement,
+                   double productQuantity, double productMinimumStock,
+                   BigDecimal productUnitCost, BigDecimal productUnitPrice,
+                   boolean isActive, LocalDateTime createAt, LocalDateTime updateAt,
+                   Category category) {
+        this.productName = productName;
+        this.productBarcode = productBarcode;
+        this.productCode = productCode;
+        this.productNcm = productNcm;
+        this.productCfop = productCfop;
+        this.productUnitOfMeasurement = productUnitOfMeasurement;
+        this.productQuantity = productQuantity;
+        this.productMinimumStock = productMinimumStock;
+        this.productUnitCost = productUnitCost;
+        this.productUnitPrice = productUnitPrice;
+        this.isActive = isActive;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+        this.category = category;
+    }
+}
