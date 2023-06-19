9c7a9fcda2c3ab0a0b9e4a62ddb29d0c204d5d17
Ricardo <rmvnew@gmail.com>
2023-06-19 18:30:36 -0400

    chore: add client controller

---
+++ src/main/java/com/delta/commerce/controller/ClientController.java
@@ -0,0 +1,27 @@
+package com.delta.commerce.controller;
+
+import com.delta.commerce.dto.request.ClientRequestDto;
+import com.delta.commerce.service.ClientService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/client")
+@Tag(name = "Client", description = "Controller para serviços de client")
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping
+    public void createClient(
+            @RequestBody ClientRequestDto dto
+            ){
+        this.clientService.createClient(dto);
+    }
+
+}
