9bcc65d7c97a34ec25580808ab0d62da10090811
Ricardo <rmvnew@gmail.com>
2023-07-21 17:39:08 -0400

    chore: complete crud customProduct

--- src/main/java/com/delta/commerce/repository/CustomerProductRepository.java
+++ src/main/java/com/delta/commerce/repository/CustomerProductRepository.java
@@ -1,7 +1,30 @@
 package com.delta.commerce.repository;
 
 import com.delta.commerce.entity.CustomerProduct;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CustomerProductRepository extends JpaRepository<CustomerProduct, Long> {
+
+
+    @Query("""
+            select c from CustomerProduct c
+            where 
+            (:brand is null or c.brand like concat('%',:brand,'%')) and
+            (:model is null or c.model like concat('%',:model,'%')) and
+            (:serialNumber is null or c.serialNumber like concat('%',:serialNumber,'%')) and
+            (:creatAt is null or c.createAt = :creatAt) and
+            c.isActive = true
+            """)
+    Page<CustomerProduct> getAllCustomerProducts(
+            @Param("brand") String brand,
+            @Param("model") String model,
+            @Param("serialNumber") String serialNumber,
+            @Param("creatAt") String creatAt,
+            Pageable page
+    );
+
 }
--- src/main/java/com/delta/commerce/service/impl/CustomerProductServiceImpl.java
+++ src/main/java/com/delta/commerce/service/impl/CustomerProductServiceImpl.java
@@ -1,10 +1,18 @@
 package com.delta.commerce.service.impl;
 
+import com.delta.commerce.dto.filter.CustomProductFilter;
 import com.delta.commerce.dto.request.CustomerProductRequestDto;
 import com.delta.commerce.entity.CustomerProduct;
+import com.delta.commerce.enums.HistoricDescriptionEnum;
+import com.delta.commerce.exception.CustomException;
+import com.delta.commerce.exception.ErrorCustom;
 import com.delta.commerce.repository.CustomerProductRepository;
 import com.delta.commerce.service.CustomerProductService;
+import com.delta.commerce.service.HistoricService;
+import com.delta.commerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,6 +22,12 @@ public class CustomerProductServiceImpl implements CustomerProductService {
 
     @Autowired
     private CustomerProductRepository customerProductRepository;
+
+    @Autowired
+    private HistoricService historicService;
+
+    @Autowired
+    private UserService userService;
 
     @Override
     public CustomerProduct createCustomProduct(CustomerProductRequestDto dto) {
@@ -26,6 +40,63 @@ public class CustomerProductServiceImpl implements CustomerProductService {
         customProduct.setActive(true);
         customProduct.setSerialNumber(dto.getSerialNumber());
 
-        return customerProductRepository.save(customProduct);
+        var customProductSaved = customerProductRepository.save(customProduct);
+
+        this.historicService.saveHistoric(
+                CustomerProduct.class, customProductSaved.getCostumerProductId(),
+                this.userService.getLoggedInUser(), HistoricDescriptionEnum.CUSTOMER_PRODUCT_CREATE);
+
+        return customProductSaved;
+
+    }
+
+    @Override
+    public CustomerProduct findById(Long id) {
+        return this.customerProductRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCustom.NOT_FOUND));
+    }
+
+    @Override
+    public Page<CustomerProduct> findAllCustomProducts(CustomProductFilter filter, Pageable page) {
+
+        String brand = filter.getBrand() != null ? filter.getBrand() : null;
+        String model = filter.getModel() != null ? filter.getModel() : null;
+        String serialNumber = filter.getSerialNumber() != null ? filter.getSerialNumber() : null;
+        String creatAt = filter.getCreatAt() != null ? filter.getCreatAt() : null;
+
+        return this.customerProductRepository.getAllCustomerProducts(brand, model, serialNumber, creatAt, page);
+    }
+
+    @Override
+    public CustomerProduct updateCustomProduct(CustomerProductRequestDto dto, Long id) {
+
+        var customProduct = this.findById(id);
+        customProduct.setUpdateAt(LocalDateTime.now());
+        customProduct.setModel(dto.getModel());
+        customProduct.setBrand(dto.getBrand());
+        customProduct.setSerialNumber(dto.getSerialNumber());
+
+        var customProductSaved = this.customerProductRepository.save(customProduct);
+
+        this.historicService.saveHistoric(
+                CustomerProduct.class, customProductSaved.getCostumerProductId(),
+                this.userService.getLoggedInUser(), HistoricDescriptionEnum.CUSTOMER_PRODUCT_UPDATE);
+
+        return customProductSaved;
+
+    }
+
+    @Override
+    public void changeStatus(Long id) {
+
+        var customProduct = this.findById(id);
+        customProduct.setActive(false);
+
+        var customProductSaved = this.customerProductRepository.save(customProduct);
+
+        this.historicService.saveHistoric(
+                CustomerProduct.class, customProductSaved.getCostumerProductId(),
+                this.userService.getLoggedInUser(), HistoricDescriptionEnum.CUSTOMER_PRODUCT_DESACTIVED);
+
     }
 }
